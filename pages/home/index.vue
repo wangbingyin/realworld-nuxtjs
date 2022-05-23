d69d9967a36ea1a70f6c0054a40bba6c3a02b52d
王丙寅 <been860413@qq.com>
2022-05-23 11:13:13 +0800

    modify title

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -3,7 +3,7 @@
 
     <div class="banner">
       <div class="container">
-        <h1 class="logo-font">La Gou</h1>
+        <h1 class="logo-font">Lao Wang</h1>
         <p>A place to share your knowledge.</p>
       </div>
     </div>
